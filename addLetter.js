2e66057797f21fca91aaffa1f8c216d1fb322d90
evan1533 <evan1533@vt.edu>
2018-11-05 16:48:10 -0500

    windows version of project

--- addLetter.js
+++ addLetter.js
@@ -1,16 +1,24 @@
-var i = 0;
-var texty = "What the swag did you just fucking yolo about me, you little wayne? I'll have you know I graduated top of my class in the SwagFags, and I've been involved in numerous Obey Records , and I have over 300 confirmed Swaggers. I am trained in wearing snapbacks and I'm the top poser in the entire Swagfag Army. You are nothing to me but just another No swag. I will swag you the fuck out with swagger the likes of which has never been seen before on this Earth, mark my fucking hashtags. You think you can get away with not taking pictures in the mirror over the Internet? Think again, fucker. As we speak I am contacting my mom, she has alot of swag, and your ratchet ass is being traced right now so you better prepare for the yolo, nikka. The yolo that wipes out the pathetic little thing you call your swag. You're fucking dead, nikka. I can swag anywhere, anytime, and I can swag in over seven hundred ways, and that's just with my baggy skinny jeans. Not only am I extensively trained in having plugs and snake bites, but I have access to the entire Hollister store. and I will use it to its full swaggness to wipe your miserable swag off the face of tumblr, you little Non trend follower. I will swag yolo all over you and you will swag in it. You're fucking dead, nikka."
+    document.getElementById("BOD").addEventListener("keypress", insertLetter)
 
-var pushText = ""
-function insertLetter() {
-    /*if(i%2==0)
-        postMessage(link1);
-    else
-        postMessage(link2);*/
-    if(i<e.data[0])
-    {
-        pushText += ("" + texty.charAt(i))
-        postMessage(pushText)
-        i++;
+
+    var texty = "What the swag did you just fucking yolo about me, you little wayne? I'll have you know I graduated top of my class in the SwagFags, and I've been involved in numerous Obey Records , and I have over 300 confirmed Swaggers. I am trained in wearing snapbacks and I'm the top poser in the entire Swagfag Army. You are nothing to me but just another No swag. I will swag you the fuck out with swagger the likes of which has never been seen before on this Earth, mark my fucking hashtags. You think you can get away with not taking pictures in the mirror over the Internet? Think again, fucker. As we speak I am contacting my mom, she has alot of swag, and your ratchet ass is being traced right now so you better prepare for the yolo, nikka. The yolo that wipes out the pathetic little thing you call your swag. You're fucking dead, nikka. I can swag anywhere, anytime, and I can swag in over seven hundred ways, and that's just with my baggy skinny jeans. Not only am I extensively trained in having plugs and snake bites, but I have access to the entire Hollister store. and I will use it to its full swaggness to wipe your miserable swag off the face of tumblr, you little Non trend follower. I will swag yolo all over you and you will swag in it. You're fucking dead, nikka."
+
+    var pushText = ""
+
+    function insertLetter() {
+        /*if(i%2==0)
+            postMessage(link1);
+        else
+            postMessage(link2);*/
+        pushText += ("" + texty.charAt(pushText.length))
+        document.getElementById("world").innerHTML = pushText;
+        $.ajax({
+            type: "POST",
+            url: "~/pythoncode.py",
+            data: {
+                param: text
+            }
+        }).done(function (o) {
+            // do something
+        });
     }
-}
--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 
-
+/*
 if (window.Worker) { // Check if Browser supports the Worker api.
     // Requires script name as input
-    var myWorker = new Worker("addLetter.js");
+    var myWorker = new Worker("worker.js");
     var txt = document.getElementById('world').innerHTML;
     myWorker.postMessage(txt.length+1);
-    console.log("PRESS")
 
     myWorker.onmessage = function (e) {
         document.getElementById('world').innerHTML = e.data;
         console.log('Message received from worker');
         addLetter();
     };
+}*/
+
+var i = 0;
+timedCount()
+function timedCount()
+{
+    i = i + 1;
+    document.getElementById("world").innerHTML = i;
+    setTimeout("timedCount()",50);
 }
